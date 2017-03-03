a9ee96071dfef39c4cc7ceb39cfc69d88aa6c4e0
IanBatesWP <IanBatesWP@gmail.com>
2017-03-02 18:32:17 -0800

    Update favicon-in-genesis.php

--- favicon-in-genesis.php
+++ favicon-in-genesis.php
@@ -1,6 +1,6 @@
 <?php
 
-/** Adding custom Favicon */
+/** Adding custom Favicon in your theme's function.php file */
 add_filter( 'genesis_pre_load_favicon', 'custom_favicon' );
 function custom_favicon( $favicon_url ) {
 	return 'http://i.imgur.com/zV9Ttdo.png';
